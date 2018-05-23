f9beb3d095239cca354457cb3d41a29416ab71d2
ctcodingstyle <ctcodingstyle@gmail.com>
2018-05-23 05:09:16 -0700

    fix #15 实现个人、企业用户登录页面

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -3,9 +3,16 @@
 '''
 # TODO 实现front.py 文件
 from flask import Blueprint, render_template
+from jobplus.forms import LoginForm
 
 front = Blueprint('front', __name__)
 
 @front.route('/')
 def index():
     return render_template('index.html')
+
+
+@front.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
